3216f7dfd4abbbc5daacb9ccf8e51b21539210de
James Chacon <chacon.james@gmail.com>
2023-02-06 22:47:43 -0800

    day3 part1
    Count up how many items are valid triangles

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,15 +1,10 @@
 //! day3 advent 20XX
 use clap::Parser;
 use color_eyre::eyre::Result;
-use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-use strum_macros::Display;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -29,6 +24,19 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut count = 0;
+    for (line_num, line) in lines.iter().enumerate() {
+        let parts = line.split_whitespace().collect::<Vec<_>>();
+        assert!(parts.len() == 3, "{} - bad line {line}", line_num + 1);
+
+        let a = parts[0].parse::<u32>().unwrap();
+        let b = parts[1].parse::<u32>().unwrap();
+        let c = parts[2].parse::<u32>().unwrap();
+
+        if a + b > c && a + c > b && b + c > a {
+            count += 1;
+        }
+    }
+    println!("part1: {count}");
     Ok(())
 }
