60c85c2103312cae747b95be9654949fe9f0ba77
James Chacon <chacon.james@gmail.com>
2023-02-06 21:51:27 -0800

    day1 part1
    Implement a basic grid walking and compute taxi distance to 0,0 when complete

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,10 +1,8 @@
 //! day1 advent 20XX
+use crate::Direction::{East, North, South, West};
 use clap::Parser;
 use color_eyre::eyre::Result;
-use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
+use grid::Location;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -21,6 +19,14 @@ struct Args {
     debug: bool,
 }
 
+#[derive(Clone, Debug, Display, PartialEq, Eq)]
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
     let args: Args = Args::parse();
@@ -29,6 +35,53 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut loc = Location(0, 0);
+    let mut dir = North;
+
+    for (line_num, line) in lines.iter().enumerate() {
+        let parts = line.split(", ").collect::<Vec<_>>();
+        for p in parts.iter().cloned() {
+            if args.debug {
+                println!("{p}");
+            }
+            assert!(
+                p.len() > 1,
+                "{} - bad direction {p} for line {line}",
+                line_num + 1
+            );
+            let mut chars = p.chars();
+            let turn = chars.nth(0).unwrap();
+            let num = chars.as_str().parse::<isize>().unwrap();
+            match turn {
+                'L' => {
+                    dir = match dir {
+                        North => West,
+                        South => East,
+                        East => North,
+                        West => South,
+                    }
+                }
+                'R' => {
+                    dir = match dir {
+                        North => East,
+                        South => West,
+                        East => South,
+                        West => North,
+                    }
+                }
+                _ => panic!("{} - bad direction {p} for line {line}", line_num + 1),
+            }
+            loc = match dir {
+                North => Location(loc.0, loc.1 + num),
+                South => Location(loc.0, loc.1 - num),
+                East => Location(loc.0 + num, loc.1),
+                West => Location(loc.0 - num, loc.1),
+            };
+            if args.debug {
+                println!("{loc}");
+            }
+        }
+    }
+    println!("part1: {}", Location(0, 0).distance(&loc));
     Ok(())
 }
