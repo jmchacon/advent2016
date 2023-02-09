c111d85f9459a2abf348fb289c9488e3e85322f2
James Chacon <chacon.james@gmail.com>
2023-02-08 23:07:12 -0800

    day4 part1
    Parse a list of string to figure out which ones are real vs decoys.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,15 +1,12 @@
 //! day4 advent 20XX
 use clap::Parser;
 use color_eyre::eyre::Result;
-use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
 use std::collections::HashMap;
+use std::fmt::Write;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-use strum_macros::Display;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -29,6 +26,70 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut real = 0;
+    for (line_num, line) in lines.iter().enumerate() {
+        let mut names = HashMap::new();
+
+        let mut sector_id = String::new();
+        let mut checksum = String::new();
+        let mut checksum_on = false;
+        for c in line.chars() {
+            match c {
+                'a'..='z' => {
+                    if !checksum_on {
+                        names.entry(c).and_modify(|v| *v += 1).or_insert(1);
+                    } else {
+                        write!(checksum, "{c}").unwrap();
+                    }
+                }
+                '0'..='9' => {
+                    write!(sector_id, "{c}").unwrap();
+                }
+                '[' => {
+                    checksum_on = true;
+                }
+                ']' | '-' => {}
+                _ => panic!("{} - bad line {line}", line_num + 1),
+            }
+        }
+        if args.debug {
+            println!("id: {sector_id}");
+            println!("checksum: {checksum}");
+            for n in &names {
+                println!("name: {n:?}");
+            }
+        }
+
+        let mut is_real = true;
+        for i in 0..checksum.chars().count() - 1 {
+            if names.contains_key(&checksum.chars().nth(i).unwrap()) {
+                if names.contains_key(&checksum.chars().nth(i + 1).unwrap()) {
+                    let x = names.get(&checksum.chars().nth(i).unwrap()).unwrap();
+                    let y = names.get(&checksum.chars().nth(i + 1).unwrap()).unwrap();
+                    if x > y {
+                        continue;
+                    } else if x == y {
+                        let c1 = checksum.chars().nth(i).unwrap();
+                        let c2 = checksum.chars().nth(i + 1).unwrap();
+                        if args.debug {
+                            println!("x: {x} y: {y} c1: {c1} c2: {c2} {}", c1 < c2);
+                        }
+                        if c1 < c2 {
+                            continue;
+                        }
+                    }
+                }
+            }
+            is_real = false;
+        }
+        if is_real {
+            if args.debug {
+                println!("real - {line}");
+            }
+            let id = sector_id.as_str().parse::<i32>().unwrap();
+            real += id;
+        }
+    }
+    println!("part1: {real}");
     Ok(())
 }
