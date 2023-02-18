681dba6a8b5c191e2cda2f8361f552d36bd05e75
James Chacon <chacon.james@gmail.com>
2023-02-17 22:20:33 -0800

    day5 part1
    m5d leading zeros problem. Iterate until we find it 8 times with a seed and record 6th position as output

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,15 +1,10 @@
 //! day5 advent 20XX
 use clap::Parser;
 use color_eyre::eyre::Result;
-use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-use strum_macros::Display;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -29,6 +24,27 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    for line in &lines {
+        let mut cur = 0;
+        let mut pass = String::new();
+        loop {
+            let digest = md5::compute(format!("{line}{cur}"));
+
+            match format!("{digest:x}").as_bytes() {
+                [b'0', b'0', b'0', b'0', b'0', x, ..] => {
+                    if args.debug {
+                        println!("Found at {cur} {digest:x}");
+                    }
+                    pass = format!("{pass}{}", core::str::from_utf8(&[*x]).unwrap());
+                    if pass.len() >= 8 {
+                        break;
+                    }
+                }
+                _ => {}
+            }
+            cur += 1;
+        }
+        println!("part1: {pass}");
+    }
     Ok(())
 }
