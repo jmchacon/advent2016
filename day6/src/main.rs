671293d93c07a343eda6697b05505590df4b1cc2
James Chacon <chacon.james@gmail.com>
2023-02-17 22:53:33 -0800

    Day6 part1
    Decode a repeated string by getting the most common letter in each column.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,15 +1,11 @@
 //! day6 advent 20XX
 use clap::Parser;
 use color_eyre::eyre::Result;
-use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-use strum_macros::Display;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -29,6 +25,32 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut cols = Vec::new();
+    let size = lines.iter().next().unwrap().len();
+    for _ in 0..size {
+        cols.push(HashMap::new());
+    }
+    for (line_num, line) in lines.iter().enumerate() {
+        assert!(line.len() == size, "{} - bad line {line}", line_num + 1);
+        for (pos, b) in line.bytes().enumerate() {
+            cols[pos]
+                .entry(b)
+                .and_modify(|v| *v += 1)
+                .or_insert(1_usize);
+        }
+    }
+    let mut out = Vec::new();
+    for c in &cols {
+        let best = c
+            .iter()
+            .map(|(k, v)| (v, k))
+            .collect::<Vec<_>>()
+            .iter()
+            .max()
+            .unwrap()
+            .clone();
+        out.push(*best.1);
+    }
+    println!("part1: {}", core::str::from_utf8(&out).unwrap());
     Ok(())
 }
