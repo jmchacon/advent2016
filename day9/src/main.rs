2ecbfc04bf5d1797833492abf95a564e67d2cdf8
James Chacon <chacon.james@gmail.com>
2023-02-22 21:31:37 -0800

    day9 part1
    string decompression. With no backup this is simply streaming forward getting data and assembling the string.

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,15 +2,11 @@
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::unwrap_used))]
 use clap::Parser;
 use color_eyre::eyre::Result;
-use grid::{Grid, Location};
-use itertools::Itertools;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
+use std::fmt::Write;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
-use strum_macros::Display;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -30,6 +26,60 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut out = String::new();
+    for line in &lines {
+        let mut c = line.chars();
+        loop {
+            let Some(cur) = c.next() else {
+                break;
+            };
+
+            // Normally we just copy char by char.
+            // Unless this is a (AxB) marker which indicates
+            // to copy the next A chars after the marker B times
+            // and then continue as before.
+            if cur == '(' {
+                let mut n = String::new();
+                // Find the number to copy.
+                loop {
+                    let cur = c.next().unwrap();
+                    if cur == 'x' {
+                        break;
+                    }
+                    write!(n, "{cur}").unwrap();
+                }
+                let num = n.parse::<usize>().unwrap();
+                n.truncate(0);
+                // Find the repeat count
+                loop {
+                    let cur = c.next().unwrap();
+                    if cur == ')' {
+                        break;
+                    }
+                    write!(n, "{cur}").unwrap();
+                }
+                let repeat = n.parse::<usize>().unwrap();
+                n.truncate(0);
+
+                // Now pull in the next num chars and save them
+                // into a string.
+                for _ in 0..num {
+                    let cur = c.next().unwrap();
+                    write!(n, "{cur}").unwrap();
+                }
+                // Copy that string repeat times into the main output string.
+                for _ in 0..repeat {
+                    write!(out, "{n}").unwrap();
+                }
+                continue;
+            }
+            write!(out, "{cur}").unwrap();
+        }
+        if args.debug {
+            println!("{line} - {out}");
+        }
+        println!("part1: {}", out.len());
+        out.truncate(0);
+    }
     Ok(())
 }
