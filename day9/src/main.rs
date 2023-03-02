4582d10c9c84b26abf6d2e696be6826d84486e70
James Chacon <chacon.james@gmail.com>
2023-03-01 20:17:31 -0800

    day9 part2
    first pass. Instead of constructing the whole string just get lengths and add together.
    
    The final one is 10G so wouldn't have fit in RAM back in 2016 likely but these could easily get a cloud instance to do it...
    
    This takes 1m53s on the surface so needs optimizing.

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -7,6 +7,7 @@ use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
+use std::str::Chars;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -26,60 +27,72 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    let mut out = String::new();
     for line in &lines {
         let mut c = line.chars();
-        loop {
-            let Some(cur) = c.next() else {
+        println!("part1: {}", outer(&mut c, false));
+        let mut c = line.chars();
+        println!("part2: {}", outer(&mut c, true));
+    }
+    Ok(())
+}
+
+fn outer(c: &mut Chars, part2: bool) -> usize {
+    let mut sz = 0;
+    loop {
+        let Some(cur) = c.next() else {
                 break;
             };
 
-            // Normally we just copy char by char.
-            // Unless this is a (AxB) marker which indicates
-            // to copy the next A chars after the marker B times
-            // and then continue as before.
-            if cur == '(' {
-                let mut n = String::new();
-                // Find the number to copy.
-                loop {
-                    let cur = c.next().unwrap();
-                    if cur == 'x' {
-                        break;
-                    }
-                    write!(n, "{cur}").unwrap();
-                }
-                let num = n.parse::<usize>().unwrap();
-                n.truncate(0);
-                // Find the repeat count
-                loop {
-                    let cur = c.next().unwrap();
-                    if cur == ')' {
-                        break;
-                    }
-                    write!(n, "{cur}").unwrap();
-                }
-                let repeat = n.parse::<usize>().unwrap();
-                n.truncate(0);
-
-                // Now pull in the next num chars and save them
-                // into a string.
-                for _ in 0..num {
-                    let cur = c.next().unwrap();
-                    write!(n, "{cur}").unwrap();
-                }
-                // Copy that string repeat times into the main output string.
-                for _ in 0..repeat {
-                    write!(out, "{n}").unwrap();
-                }
-                continue;
-            }
-            write!(out, "{cur}").unwrap();
+        // Normally we just copy char by char.
+        // Unless this is a (AxB) marker which indicates
+        // to copy the next A chars after the marker B times
+        // and then continue as before.
+        if cur == '(' {
+            sz += process(c, part2);
+            continue;
         }
-        if args.debug {
-            println!("{line} - {out}");
-        }
-        println!("part1: {}", out.len());
-        out.truncate(0);
+        sz += 1;
     }
-    Ok(())
+    sz
+}
+
+fn process(c: &mut Chars, part2: bool) -> usize {
+    let mut out = String::new();
+    let mut n = String::new();
+    // Find the number to copy.
+    loop {
+        let cur = c.next().unwrap();
+        if cur == 'x' {
+            break;
+        }
+        write!(n, "{cur}").unwrap();
+    }
+    let num = n.parse::<usize>().unwrap();
+    n.truncate(0);
+    // Find the repeat count
+    loop {
+        let cur = c.next().unwrap();
+        if cur == ')' {
+            break;
+        }
+        write!(n, "{cur}").unwrap();
+    }
+    let repeat = n.parse::<usize>().unwrap();
+    n.truncate(0);
+
+    // Now pull in the next num chars and save them
+    // into a string.
+    for _ in 0..num {
+        let cur = c.next().unwrap();
+        write!(n, "{cur}").unwrap();
+    }
+    // Copy that string repeat times into the main output string.
+    for _ in 0..repeat {
+        write!(out, "{n}").unwrap();
+    }
+    if part2 && out.contains('(') {
+        let mut c = out.chars();
+        return outer(&mut c, part2);
+    }
+    out.len()
 }
